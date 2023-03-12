31fd85e7058603d5e07bebeb7e6e1610aa990956
JoaoMaciel5G <juauvitu765@gmail.com>
2023-03-12 16:02:51 -0300

    add sidebar and finally project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,21 @@
 "use client";
 
-import Link from 'next/link'
-import {AiOutlineMenu} from "react-icons/ai"
-import '../styles/globals.css'
+import Link from "next/link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import "../styles/globals.css";
+import { useRef } from "react";
 
 export default function RootLayout({ children }) {
+
+  const refSideBar = useRef();
+
+  function openSideBar() {
+    refSideBar.current.style.width = "250px"
+  }
+
+  function closeSideBar(){
+    refSideBar.current.style.width = "0px"
+  }
   return (
     <html lang="pt-br">
       <head />
@@ -14,12 +25,18 @@ export default function RootLayout({ children }) {
             <h2>JM Neurociências</h2>
           </div>
           <nav className="navBar">
+            <Link href="/">Página principal</Link>
             <Link href="/posts">Artigos</Link>
           </nav>
-          <AiOutlineMenu id="icon"/>
+          <AiOutlineMenu className="icon" onClick={openSideBar} />
         </header>
+        <div className="sidenav" ref={refSideBar}>
+          <AiOutlineClose className="closeSideNav" onClick={closeSideBar}/>
+          <Link onClick={closeSideBar} href="/">Página principal</Link>
+          <Link onClick={closeSideBar} href="/posts">Artigos</Link>
+        </div>
         {children}
       </body>
     </html>
-  )
+  );
 }
